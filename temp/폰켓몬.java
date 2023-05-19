35fbfdbe0f8fe344b5cb04125fcf109e62c49211
TaeHwan Jung <jth8854@gmail.com>
2023-05-19 17:08:29 +0900

    test

--- temp/폰켓몬.java
+++ temp/폰켓몬.java
@@ -4,14 +4,15 @@ import java.util.Set;
 class Solution {
     public int solution(int[] nums) {
         Set<Integer> tempSet = new HashSet<>();
+        int limitCnt = nums.length / 2;
         
-        for (int number : nums) {
-            tempSet.add(number);
-            if (tempSet.size() >= nums.length / 2) {
+        for (int num : nums) {
+            tempSet.add(num);
+            if (tempSet.size() == limitCnt) {
                 break;
             }
         }
         
-        return Math.min(tempSet.size(), nums.length / 2);
+        return Math.min(tempSet.size(), limitCnt);
     }
 }
