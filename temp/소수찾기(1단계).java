27189f5729c40377261c709e7876c7944a08167e
jungtaehwan <82363795+taehwanis@users.noreply.github.com>
2023-06-13 11:12:20 +0900

    Update 소수찾기(1단계).java

--- temp/소수찾기(1단계).java
+++ temp/소수찾기(1단계).java
@@ -5,7 +5,7 @@ class Solution {
         
         for(int i = 3; i <= n; i += 2){
             trigger = true;
-            for(int j = 3; j <= Math.sqrt(i); j+=2){
+            for(int j = 3; j <= Math.sqrt(i); j += 2){
                 if(i % j == 0){
                     trigger = false;
                     break;
