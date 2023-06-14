fd2dbb9a75cac69cb5d311829f64d3d870d0bdfd
TaeHwan Jung <jth8854@gmail.com>
2023-06-14 10:57:25 +0900

    test

--- temp/소수찾기(1단계).java
+++ temp/소수찾기(1단계).java
@@ -3,18 +3,24 @@ class Solution {
         int answer = 1;
         boolean trigger = true;
         
-        for(int i = 3; i <= n; i += 2){
-            trigger = true;
-            for(int j = 3; j <= Math.sqrt(i); j += 2){
-                if(i % j == 0){
-                    trigger = false;
-                    break;
+        if(n > 2){
+            for(int i = 3; i <= n; i += 2){
+                trigger = true;
+
+                for(int j = 3; j <= Math.sqrt(i); j += 2){
+
+                    if(i % j == 0){
+                        trigger = false;
+                        break;
+                    }
+                }
+                
+                if(trigger){
+                    answer++;
                 }
             }
-            if(trigger){
-                answer++;
-            }
         }
+
         return answer;
     }
 }
