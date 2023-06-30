e187db96399f115cffe5ff02f602667bd52465b7
TaeHwan Jung <jth8854@gmail.com>
2023-06-30 15:27:25 +0900

    test

--- temp/실패율.java
+++ temp/실패율.java
@@ -37,4 +37,41 @@ class Solution {
 
         return result;
     }
+
+
+    public int[] selectBdgExeN(int N, int[] stages) {
+        Map<Integer, Double> exeBizObj = new HashMap<>();
+
+        double len = stages.length;
+
+        for (int i = 0; i < stages.length; i++) {
+            if(stages[i] < N + 1){
+                exeBizObj.put(stages[i], exeBizObj.getOrDefault(stages[i], 0.0) + 1);
+            }
+        }
+
+        for (int stg = 1; stg <= N; stg++) {
+            double cnt = exeBizObj.getOrDefault(stg, 0.0);
+            if(cnt == 0.0){
+                exeBizObj.put(stg, cnt);
+            }else{
+                exeBizObj.put(stg, exeBizObj.get(stg) / len);
+                len -= cnt;
+            }
+
+        }
+
+        List<Map.Entry<Integer, Double>> entries = new ArrayList<>(exeBizObj.entrySet());
+
+        Collections.sort(entries, Comparator.comparing(Map.Entry::getValue, Comparator.reverseOrder()));
+
+
+        int[] answer = new int[N];
+
+        for (int i = 0; i < answer.length; i++) {
+            answer[i] = entries.get(i).getKey();
+        }
+
+        return answer;
+    }
 }
