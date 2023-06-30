0b025c92a2cd787da75ae019a9ebdbd54ad6d20f
TaeHwan Jung <jth8854@gmail.com>
2023-06-30 15:16:31 +0900

    test

---
+++ temp/실패율.java
@@ -0,0 +1,40 @@
+import java.util.*;
+
+
+class Solution {
+
+    public int[] selectBdgExeN(int N, int[] stages) {
+        Map<Integer, Map<String, Double>> exeBizObj = new HashMap<>();
+
+        double len = stages.length;
+
+        for (int i = 0; i < stages.length; i++) {
+            if(stages[i] == N + 1) continue;
+            Map<String, Double> tempObj = exeBizObj.getOrDefault(stages[i], new HashMap<>());
+            tempObj.put("cnt", tempObj.getOrDefault("cnt", 0.0) + 1);
+            exeBizObj.put(stages[i], tempObj);
+        }
+
+        for (int stg = 1; stg <= N; stg++) {
+            Map<String, Double> tempObj = exeBizObj.getOrDefault(stg, new HashMap<>());
+            tempObj.put("cnt", tempObj.getOrDefault("cnt", 0.0));
+            tempObj.put("rate", tempObj.get("cnt") / len);
+            exeBizObj.put(stg, tempObj);
+
+            len -= tempObj.get("cnt");
+        }
+
+        List<Map.Entry<Integer, Map<String, Double>>> entries = new ArrayList<>(exeBizObj.entrySet());
+
+        Collections.sort(entries, Comparator.comparing(entry -> entry.getValue().get("rate"),Comparator.reverseOrder()));
+
+
+        int[] result = new int[N];
+
+        for (int i = 0; i < result.length; i++) {
+            result[i] = entries.get(i).getKey();
+        }
+
+        return result;
+    }
+}
