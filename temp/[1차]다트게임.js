a140347f8afcb65c38e96d9bbd454a7978229979
TaeHwan Jung <jth8854@gmail.com>
2023-07-04 16:26:08 +0900

    test

--- temp/[1차]다트게임.js
+++ temp/[1차]다트게임.js
@@ -6,18 +6,37 @@ function solution(dartResult) {
 	const procArr = dartResult.match(/(\d+\D+)/g).reduce((arr, item, idx) => {
 
 		const len = item.startsWith('10') ? 2 : 1;
-
-		arr.push(item.substring(0, len) ** bonus[item[len]]);
 		const optionVal = option[item[len+1]] ?? 1;
+
+		arr.push(item.substring(0, len) ** bonus[item[len]] * optionVal);
 
 		if (!(optionVal < 2 || idx == 0)) {
 			arr[idx - 1] *= optionVal;
 		}
 
-		arr[idx] *= optionVal;
-
 		return arr;
 	}, []);
 
 	return procArr.reduce((acc, el) => acc + el, 0);
+}
+
+function solutionB(dartResult){
+	const bonus = {'S': 1, 'D': 2, 'T': 3};
+	const option = {'*': 2, '#': -1};
+
+	let len, optionVal, resultArr = [];
+	const procArr = dartResult.match(/(\d+\D+)/g);
+
+	for (let i = 0; i < procArr.length; i++) {
+		len = procArr[i].startsWith('10') ? 2 : 1;
+		optionVal = option[procArr[i][len+1]] ?? 1;
+
+		resultArr.push(procArr[i].substring(0, len) ** bonus[procArr[i][len]] * optionVal);
+
+		if (!(optionVal < 2 || i == 0)) {
+			resultArr[i - 1] *= optionVal;
+		}
+	}
+
+	return resultArr.reduce((acc, el) => acc + el, 0);
 }
