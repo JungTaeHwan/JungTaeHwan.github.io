6208e0a579b4450926e99c909782cda36da0923d
TaeHwan Jung <jth8854@gmail.com>
2023-08-18 14:43:12 +0900

    test

--- temp/덧칠하기.js
+++ temp/덧칠하기.js
@@ -9,4 +9,26 @@ function solution(n, m, section) {
                 
         return acc;
     }, 0);
+
+     /* let result = 0;
+            let temp = 0;
+
+            for (let i = 0; i < section.length; i = temp) {
+                let next = i + 1;
+                while(next < section.length){
+                    if(section[next] - section[i] < m){
+                        next++;
+                    }else{
+                        result++;
+                        break;
+                    }
+                }
+                temp = next;
+
+                if(next == section.length){
+                    result++;
+                }
+            }
+
+            return result; */
 }
