0440a58d94ea1d1631d9794be124a6d7aecc1dbb
taehwanis <wjdxo2546@naver.com>
2023-08-23 01:32:41 +0900

    test

---
+++ temp/기사단원의 무기.js
@@ -0,0 +1,25 @@
+function solution(number, limit, power) {
+	let answer = 0;
+
+	function getCnt(num){
+		let cnt = 0;
+
+		for (let i = 1; i <= Math.sqrt(num); i++) {
+			if(num % i == 0){
+                i == Math.sqrt(num) ? cnt++ : cnt += 2;
+			}
+
+            if(cnt > limit){
+                return power;
+            }
+		}
+
+		return cnt;
+	}
+
+	for (let i = 1; i <= number; i++) {
+		answer += getCnt(i);
+	}
+
+	return answer;
+}
