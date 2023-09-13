6da40785e989fc28a820b2c1ec3a8a6d135ae975
TaeHwan Jung <jth8854@gmail.com>
2023-09-13 12:10:37 +0900

    prog11, 12

--- temp/마법의 엘리베이터.js
+++ temp/마법의 엘리베이터.js
@@ -1,10 +1,18 @@
 
 function solution(storey) {
-    let cnt = 0;
+
+
+    
+
+
+
+
+
+    /* let cnt = 0;
     const temp = [...(storey).toString()];
 
     for (let i = temp.length - 1; i >= 0; i--) {
-        if (temp[i] >= 5) {
+        if (temp[i] >= 5) { 
             cnt += 10 - Number(temp[i]);
 
             if (i ==0) {
@@ -12,7 +20,7 @@ function solution(storey) {
                     cnt++;
                 }
             } else {
-                if(!(temp[i] == 5 && temp[i-1] < 5)){
+                if(temp[i] != 5 || temp[i-1] >= 5){
                     temp[i - 1]++;
                 }
             }
@@ -21,5 +29,5 @@ function solution(storey) {
         }
     }
 
-    return cnt;
+    return cnt; */
 }
--- temp/체육복.js
+++ temp/체육복.js
@@ -1,18 +1,21 @@
 function solution(n, lost, reserve) {
     let temp = reserve;
 
+    //sort해서 앞에서부터 훔치기
 	lost.sort().forEach(el => {
-        if(reserve.includes(el)){
-            temp = temp.filter(item => el != item);
-        }else if(!reserve.includes(el) && temp.includes(el-1)){
+        if(reserve.includes(el)){ //체육복 부자
+            temp = temp.filter(item => el != item); //filter로 제외시키기
+        }else if(!reserve.includes(el) && temp.includes(el-1)){ // 뒷사람 체육복 훔치기
             temp = temp.filter(item => el != item +1);
-        }else if(!reserve.includes(el) && temp.includes(el+1)){
+        }else if(!reserve.includes(el) && temp.includes(el+1)){ // 앞사람 체육복 훔치기
             temp = temp.filter(item => el != item -1);
         }
     });
 
-    return n - lost.length + reserve.length - temp.length;
+    return n - lost.length + reserve.length - temp.length; // n - 안가져온 사람 + (실제로 줄 수 있는 사람)
 
+
+    
     /* 	const gymSuit = Array(n).fill(1);
     
     lost.forEach(el => gymSuit[el-1]--);
