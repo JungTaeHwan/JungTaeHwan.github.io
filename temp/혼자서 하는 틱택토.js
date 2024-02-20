965dfb610b0f8ba00205bbceac40ddea9a8683e4
TaeHwan Jung <jth8854@gmail.com>
2024-02-20 11:55:08 +0900

    test

--- temp/혼자서 하는 틱택토.js
+++ temp/혼자서 하는 틱택토.js
@@ -47,3 +47,111 @@ function solution(board) {
     }
 
 }
+
+
+
+
+
+
+
+
+
+
+
+
+function solution(board) {
+    const getCheckSheet = (len) => {
+        const [horizon, vertical, majorDiag, antiDiag] = [[], [], [], []];
+
+        for (let i = 0; i < len; i++) {
+            horizon.push(Array.from({length: len}, (_, idx) => i * len + idx))
+            vertical.push(Array.from({length: len}, (_, idx) => i + len * idx))
+            majorDiag.push(i * (len + 1));
+            antiDiag.push((i + 1) * (len - 1));
+        }
+
+        const mainSheet = [...horizon, ...vertical];
+        mainSheet.push(majorDiag, antiDiag);
+
+        return mainSheet;
+    }
+
+    const winOrNot = (arr) => {
+        return checkSheet.some(answer => answer.every(el => arr.includes(el)));
+    }
+
+    const checkSheet = getCheckSheet(board.length);
+
+    const myBoard = board.flatMap(el => el.split(''));
+    
+    const {o, x} = myBoard.reduce((obj, el, idx) => {
+        if (el == 'O') {
+            obj.o.push(idx);
+        } else if (el == 'X') {
+            obj.x.push(idx);
+        }
+        return obj;
+    }, {o: [], x: []});
+
+    const diff = o.length - x.length;
+
+    if(diff == 0){
+        return winOrNot(o) ? 0 : 1;
+    }else if (diff == 1){
+        return winOrNot(x) ? 0 : 1;
+    }else{
+        return 0;
+    }
+
+}
+
+
+
+
+
+function solution(board) {
+    const getCheckSheet = (len) => {
+        const [horizon, vertical, majorDiag, antiDiag] = [[], [], [], []];
+
+        for (let i = 0; i < len; i++) {
+            horizon.push(Array.from({length: len}, (_, idx) => i * len + idx))
+            vertical.push(Array.from({length: len}, (_, idx) => i + len * idx))
+            majorDiag.push(i * (len + 1));
+            antiDiag.push((i + 1) * (len - 1));
+        }
+
+        const mainSheet = [...horizon, ...vertical];
+        mainSheet.push(majorDiag, antiDiag);
+
+        return mainSheet;
+    }
+
+    const winOrNot = (arr) => {
+        return checkSheet.some(answer => answer.every(el => arr.includes(el)));
+    }
+
+    const checkSheet = getCheckSheet(board.length);
+
+    const {o, x} = board.flatMap(el => el.split(''))
+        .reduce((obj, el, idx) => {
+            if (el == 'O') {
+                obj.o.push(idx);
+            } else if (el == 'X') {
+                obj.x.push(idx);
+            }
+            return obj;
+        }, {o: [], x: []});
+
+    const diff = o.length - x.length;
+    
+    if(winOrNot(o) && !winOrNot(x) && diff == 1){
+        return 1;
+    }else if (!winOrNot(o) && winOrNot(x) && diff == 0){
+        return 1;
+    }else if (!winOrNot(o) && !winOrNot(x) && [0, 1].includes(diff)){
+        return 1;
+    }else{
+        return 0;
+    }
+
+}
