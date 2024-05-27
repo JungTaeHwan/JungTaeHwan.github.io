be66bc43503de096da37e67b64f191b7036d07b5
TaeHwan Jung <jth8854@gmail.com>
2024-05-27 18:01:10 +0900

    test

--- temp/유사 칸토어 비트열.js
+++ temp/유사 칸토어 비트열.js
@@ -49,3 +49,12 @@ function solution(n, l, r) {
 
     return result.reduce((acc, el) => acc += el, 0);
 }
+
+
+function solution(n, l, r) {
+    let answer = 0;
+    for (let i = l - 1; i <= r - 1; i++) {
+        if (!i.toString(5).match('2')) answer += 1;
+    }
+    return answer;
+}
