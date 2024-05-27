ad3cd587e7b02150c001a8b8e41db6b81702586e
TaeHwan Jung <jth8854@gmail.com>
2024-05-27 17:41:18 +0900

    prog47

---
+++ temp/유사 칸토어 비트열.js
@@ -0,0 +1,51 @@
+function solution(n, l, r) {
+    const result = [];
+
+    const dfs = (lvl, left, right) => {
+        const area = 5 ** (lvl - 1);
+
+        if (lvl == 1) {
+            result.push([1, 1, 0, 1, 1].reduce((acc, el, idx) => {
+                if (idx >= left - 1 && idx <= right - 1) {
+                    acc += el;
+                }
+
+                return acc;
+            }, 0));
+
+            return;
+        }
+
+        let alpha = 1, beta = 1;
+
+        while (left > (area * alpha)) {
+            alpha++;
+        }
+
+        while (right > (area * beta)) {
+            beta++;
+        }
+
+
+        result.push([1, 2, 3, 4, 5].filter(el => el > alpha && el < beta).reduce((acc, el) => acc += el == 3 ? 0 : 4 ** (lvl - 1), 0));
+
+        if (alpha == beta) {
+            if (alpha == 3) {
+                return;
+            }
+            return dfs(lvl - 1, left - (area * (beta - 1)), right - (area * (beta - 1)), result);
+        } else {
+            if (alpha != 3 && beta == 3) {
+                return dfs(lvl - 1, left - (area * (alpha - 1)), area, result);
+            } else if (alpha == 3 && beta != 3) {
+                return dfs(lvl - 1, 1, right - area * (beta - 1), result);
+            } else {
+                return dfs(lvl - 1, left - (area * (alpha - 1)), area, result), dfs(lvl - 1, 1, right - area * (beta - 1), result);
+            }
+        }
+    }
+
+    dfs(n, l, r);
+
+    return result.reduce((acc, el) => acc += el, 0);
+}
